8447ab8467337b7d89fd86cdf128d7ae937bc9be
Guillermo Ramirez <gramirez@cidenet.com.co>
2023-08-27 11:44:40 -0500

    Comiit de mis primeros hooks

---
+++ useTodos.js
@@ -0,0 +1,71 @@
+import { useEffect, useReducer } from "react"
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+const initialStage = [
+    // {
+    //     id: new Date().getTime(),
+    //     description: 'Recolectar la piedra del Alma',
+    //     done: false
+    // },
+    // {
+    //     id: new Date().getTime() * 3,
+    //     description: 'Recolectar la piedra del poder',
+    //     done: false
+    // }
+];
+
+const init = () =>{
+    return JSON.parse(localStorage.getItem('todos')) || [];
+}
+export const useTodos = () => {
+
+
+    const [todos, dispatch] = useReducer(todoReducer, initialStage, init);
+
+    useEffect(() => {
+      localStorage.setItem('todos', JSON.stringify(todos))
+    
+    }, [todos])
+    
+
+    const handleNewTodo = (todo) => {
+    console.log({todo});
+        const action = {
+            type: '[TODO] add todo',
+            payload: todo
+        };
+        
+    dispatch( action );
+    };
+
+    const handleRemoveTodo = ( id ) => {
+        // console.log(id);
+        dispatch({
+            type: 'TODO Remove todo',
+            payload: id
+        });
+    };
+
+    const handleToggleTodo = (id) => {
+        // console.log(id);
+        dispatch({
+            type: 'TODO Toggle todo',
+            payload: id
+        });
+    }
+    const todosCount = todos.length
+
+    const pendingTodosCount = todos.filter(todo => !todo.done).length
+ 
+
+
+    return {
+        // ...todos,
+        todos,
+        handleNewTodo,
+        handleRemoveTodo,
+        handleToggleTodo,
+        todosCount,
+        pendingTodosCount
+      }
+}
